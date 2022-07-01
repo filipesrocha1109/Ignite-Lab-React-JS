061ec158edf0f285702ab2a777a34ccefc6380c6
Filipe Rocha <fsrbillie@gmail.com>
2022-07-01 15:05:07 -0300

    aula-04

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri:'https://api-sa-east-1.graphcms.com/v2/cl4oh8doy03bv01xn1z0l4v5j/master',
-    cache: new InMemoryCache()
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
+  cache: new InMemoryCache()
 })
